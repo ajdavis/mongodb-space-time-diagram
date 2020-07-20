cda901b2ff7a633de5ea8e93aedaf4179c316cff
A. Jesse Jiryu Davis <jesse@mongodb.com>
2020-07-19 20:01:06 -0400

    start parsing messages
    I was trying to detect log messages indicating that a heartbeat request or reply was sent or received, so I could correlate them. This doesn't yet account for mongobridge (it fails with an assertion, since the JS test prefixes with e.g. "d20030" a message from a host that thinks it's on port 20020, due to mongobridge). In any case it's the wrong approach, Shiviz doesn't need send/receive events, it just needs to know when a vector clock changes. I'm going to implement a vector clock for MongoDB messages and resume.

--- bf_log_to_shiviz/bf_log_to_shiviz.py
+++ bf_log_to_shiviz/bf_log_to_shiviz.py
@@ -21,7 +21,7 @@ Timestamp:
     '+0000'
 ;
 
-HostID: /(d|s|m)\d+\|/;
+HostID: hostType=/d|s|m/ port=INT '|';
 
 JsonOrText: Json | Text;
 
@@ -34,7 +34,7 @@ OtherLine: message=/.*$/;
 
 
 def process_timestamp(obj):
-    obj.timestamp = datetime.datetime(
+    return datetime.datetime(
         int(obj.year), int(obj.month), int(obj.day), int(obj.hour),
         int(obj.minute), int(obj.second), 1000 * int(obj.millis))
 
@@ -54,17 +54,84 @@ mm.register_obj_processors(obj_processors)
 model = mm.model_from_file(sys.argv[1])
 
 
+def dict_get(dct, *path):
+    for p in path:
+        if p in dct:
+            dct = dct[p]
+        else:
+            return None
+
+    return dct
+
+
 @singledispatch
-def process(line):
-    print(f'process({line})')
+def process(line, lineno):
+    raise ValueError(f'Cannot process line {lineno}: {line}')
+
+
+# https://github.com/DistributedClocks/shiviz/wiki
+# ShiViz parses the log using a user-specified regular expression. The regular
+# expression must contain three capture groups:
+#
+# event: The event message
+# host: The host / process for the event
+# clock: The vector clock, in JSON {"host": timestamp} format. The local host
+# must be represented in the vector clock (i.e. the host specified in the host
+# capture groups must be one of the hosts in the vector clock).
+@process.register
+def process_jstest_line(line: mm['JSTestLine'], lineno):
+    print(f'process_jstest_line({line})')
+    if line.message.__class__.__name__ == 'Text':
+        print(f'{line.timestamp} {line.message}')
+        return
+
+    msg_id = line.message.parsed['id']
+    # 'Sending heartbeat'.
+    if msg_id == 4615670:
+        source = line.message.parsed['attr']['heartbeatObj']['from']
+        # TODO: Why is source sometimes empty?
+        if not source:
+            return
+
+        source_port = int(source.split(':')[1])
+        assert source_port == line.hostID.port
+        target_port = line.message.parsed['attr']['target'].split(':')[1]
+        print(f'{line.timestamp} Send HB'
+              f' from {source_port} to {target_port}')
+    # 'Received heartbeat request'.
+    elif msg_id == 24095:
+        source = line.message.parsed['attr']['from']
+        if not source:
+            return
+
+        source_port = source.split(':')[1]
+        print(f'{line.timestamp} Receive HB'
+              f' from {source_port} to {line.hostID.port}')
+    # 'Generated heartbeat response'.
+    elif msg_id == 24097:
+        source = line.message.parsed['attr']['from']
+        if not source:
+            return
+
+        source_port = source.split(':')[1]
+        print(f'{line.timestamp} Reply to HB'
+              f' from {source_port} to {line.hostID.port}')
+    # 'Received response to heartbeat'.
+    elif msg_id == 4615620:
+        # Yes, the heartbeat reply's source is called 'target'.
+        source = line.message.parsed['attr']['target']
+        source_port = source.split(':')[1]
+        print(f'{line.timestamp} Receive HB reply'
+              f' from {source_port} to {line.hostID.port}')
 
 
 @process.register
-def process_jstest_line(line: mm['JSTestLine']):
-    print(f'process_jstest_line({line})')
-    if line.message.__class__.__name__ == 'Json':
-        print(line.message.parsed)
+def process_other_line(line: mm['OtherLine'], lineno):
+    print(line.message)
 
 
-for l in model.lines:
-    process(l)
+for i, line in enumerate(model.lines, start=1):
+    try:
+        process(line, i)
+    except Exception as exc:
+        print(f'{exc!r} parsing line {i}: {line}')
